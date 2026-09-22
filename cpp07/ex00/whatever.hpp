2d81596b2f126a6381c26401b61d1fa8c43df9a4
bamberger12 <edu70347@gmail.com>
2026-09-22 16:55:45 +0200

    cpp07/ex00

---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+template <typename T>
+void	swap(T &a, T &b)
+{
+	T	c = a;
+	a = b;
+	b = c;
+}
+
+template <typename T>
+T	max(T &a, T &b)
+{
+	if (a <= b)
+		return (b);
+	return (a);
+}
+
+template <typename T>
+T	min(T &a, T &b)
+{
+	if (a >= b)
+		return (b);
+	return (a);
+}
